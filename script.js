5bd7a0579c0eecf341b064341c9ce1eea701b193
Micah Yunana Bala <micahkulien@gmail.com>
2018-10-19 11:05:39 +0100

    Get and Display Data from a separate JSON file

--- script.js
+++ script.js
@@ -1,34 +1,47 @@
-// Create a JSON file
-let info = {
-  full_name: 'Micah Bala',
-  title: 'Mr.',
-  links: [
-    { LinkedIn: 'https://linkedin/in/micahbala' },
-    { facebook: 'https://facebook.com/micahbala' },
-    { Twitter: 'https://twitter.com/micahbala' },
-    { GitHub: 'https://github.com' }
-  ]
-};
+// AJAX Request
+let request;
 
-let myName = `${info.full_name}`;
-let output = '';
+// With AJAX you create a request that goes to the server but it is handled differently by older microsoft browsers the next lines of code checks to see if the browser uses an HMLHttpRequest also known as an XHR request and places that request into the request variable
 
-// Loop through the links object
-for (let i = 0; i < info.links.length; i++) {
-  // for each of the links object pick the key
-  for (key in info.links[i]) {
-    // Verify that no other properties have been added to this object, if it has the property we are looking for
-    if (info.links[i].hasOwnProperty(key)) {
-      output += `<li><a href="${info.links[i][key]}">${key}</a></li>`;
-    } //hasOwnProperty check
-  } //for each object
-} //for each array element
+if (window.XMLHttpRequest) {
+  request = new XMLHttpRequest();
+} else {
+  request = new ActiveXObject('Microsoft.HMLHTTP');
+}
 
-// Get the DOM element
-let update = document.getElementById('links');
-let name = document.querySelector('.name');
+// Open the request asking for a specific file
+request.open('GET', 'data.json');
 
-name.innerText = myName;
+// Event handler to monitor the status of the request we are going to make. check to see if the request is successful by checking some variables the server will return
+request.onreadystatechange = function() {
+  if (request.status === 200 && request.readyState === 4) {
+    // Bring the requested data into our object, parsing the response text
+    let info = JSON.parse(request.responseText);
 
-// set the inner html to the output variable
-update.innerHTML = output;
+    let myName = `${info.full_name}`;
+    let output = '';
+
+    // Loop through the links object
+    for (let i = 0; i < info.links.length; i++) {
+      // for each of the links object pick the key
+      for (key in info.links[i]) {
+        // Verify that no other properties have been added to this object, if it has the property we are looking for
+        if (info.links[i].hasOwnProperty(key)) {
+          output += `<li><a href="${info.links[i][key]}">${key}</a></li>`;
+        } //hasOwnProperty check
+      } //for each object
+    } //for each array element
+
+    // Get the DOM element
+    let update = document.getElementById('links');
+    let name = document.querySelector('.name');
+
+    name.innerText = myName;
+
+    // set the inner html to the output variable
+    update.innerHTML = output;
+  } //ready
+}; //event
+
+// We are now sending the request
+request.send();
